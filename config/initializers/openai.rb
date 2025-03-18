5b468aeb0a20eec5bf053c1e4b4236f6e489a506
Roman Borysenok <roman.borysenok@gmail.com>
2025-03-18 16:05:01 +0100

    init

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,7 @@
-OpenAI.configure do |config|
-  config.access_token = ENV.fetch('OPENAI_API_KEY')
-  config.organization_id = ENV['OPENAI_ORGANIZATION_ID'] if ENV['OPENAI_ORGANIZATION_ID'].present?
-  config.request_timeout = ENV.fetch('OPENAI_REQUEST_TIMEOUT', 120).to_i
+if ENV['OPENAI_API_KEY'].present?
+  OpenAI.configure do |config|
+    config.access_token = ENV['OPENAI_API_KEY']
+    config.organization_id = ENV['OPENAI_ORGANIZATION_ID'] if ENV['OPENAI_ORGANIZATION_ID'].present?
+    config.request_timeout = ENV.fetch('OPENAI_REQUEST_TIMEOUT', 120).to_i
+  end
 end 
